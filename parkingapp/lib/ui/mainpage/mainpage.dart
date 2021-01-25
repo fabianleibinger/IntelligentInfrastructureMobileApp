7f80485f8d188a10477015cc0727410214c7839e
Carl Hauschke <info@carlhauschke.de>
2021-01-25 11:44:47 +0100

    add mainpage body
    basic implementation of mainpage body:
    parking garage title and image
    static vehicle-specific information

--- parkingapp/lib/ui/mainpage/mainpage.dart
+++ parkingapp/lib/ui/mainpage/mainpage.dart
@@ -7,6 +7,15 @@ import 'package:parkingapp/models/classes/user.dart';
 import 'package:parkingapp/models/global.dart';
 import 'package:wifi/wifi.dart';
 
+final parkhausImage = 'assets/parkgarage-fasanengarten.jpg';
+final parkhausImageHeight = 250;
+final bottomMargin = 220;
+List<String> _properties = [
+  'Freie Parkplätze: 79',
+  'Fahrzeugpräferenzen: keine'
+];
+bool _charge = false;
+
 class MainPage extends StatefulWidget {
   final String apikey;
 
@@ -19,39 +28,96 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Vehicle', style: whiteHeader),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: green,
+        appBar: AppBar(
+          title: Text('Vehicle', style: whiteHeader),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: green,
+                ),
+                child: Text('Parkhaus', style: blackHeader),
               ),
-              child: Text('Parkhaus', style: blackHeader),
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: Text('Fahrzeug einparken'),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        body: Column(
+          children: [
+            Container(
+              constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height - bottomMargin),
+              child: Card(
+                margin: EdgeInsets.all(0),
+                elevation: 10,
+                clipBehavior: Clip.antiAlias,
+                child: Column(
+                  children: [
+                    ListTile(
+                      //leading: Icon(Icons.location_on),
+                      title: const Text('Parkgarage Fasanengarten'),
+                      subtitle: Text(
+                        'Tiefgarage',
+                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                      ),
+                    ),
+                    Container(
+                      height: parkhausImageHeight.toDouble(),
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage(parkhausImage),
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    Flexible(
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: buildElements(_properties),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: Text('Fahrzeug einparken'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+        ));
+  }
+
+  List<Widget> buildElements(List<String> elements) {
+    List<Widget> widgets = [];
+    widgets.addAll(elements
+        .map((element) => ListTile(
+              title: Text(element),
+            ))
+        .toList());
+    widgets.add(SwitchListTile(
+      title: Text('Fahrzeug laden'),
+      onChanged: (bool newValue) {
+        setState(() {
+          _charge = newValue;
+        });
+      },
+      value: _charge,
+    ));
+    return widgets;
   }
 }
