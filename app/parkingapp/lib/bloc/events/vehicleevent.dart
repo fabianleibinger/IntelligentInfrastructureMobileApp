0c2eb9773dcf6b774050c9c1ae3b59005dd1f08a
uyhwq <uyhwq@student.kit.edu>
2021-01-27 16:47:02 +0100

    unfinished blocStructur & DB 3.0

---
+++ app/parkingapp/lib/bloc/events/vehicleevent.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+import 'package:parkingapp/models/classes/vehicle.dart';
+
+enum EventType { add, delete, update }
+
+class VehicleEvent {
+  Vehicle vehicle;
+  int vehicleIndex;
+  EventType eventType;
+
+  VehicleEvent.add(Vehicle vehicle) {
+    this.eventType = EventType.add;
+    this.vehicle = vehicle;
+  }
+
+  VehicleEvent.delete(int index) {
+    this.eventType = EventType.delete;
+    this.vehicleIndex = index;
+  }
+
+  VehicleEvent.modify(int index, Vehicle vehicle) {
+    this.eventType = EventType.update;
+    this.vehicleIndex = index;
+  }
+}
