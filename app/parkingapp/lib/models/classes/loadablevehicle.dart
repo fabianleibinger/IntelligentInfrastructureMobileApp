73a8a12680cb5e372d6257fb92043b75ccfe608c
Fabian <fabian_leibinger@gmx.de>
2021-01-29 13:00:38 +0100

    refactored vehicle.dart, 4 variables instead of one List for dimensions, 2 variables instead of one List for preferences, outsourced loadablevehicle.dart (new name for electricalVehicle)

---
+++ app/parkingapp/lib/models/classes/loadablevehicle.dart
@@ -0,0 +1,42 @@
+import 'package:parkingapp/models/classes/vehicle.dart';
+
+class LoadableVehicle extends Vehicle {
+  bool doCharge;
+  String chargingProvider;
+  List<DateTime> chargeTime;
+  String charge;
+
+  LoadableVehicle(key, name, licensePlate, height, width, length, turningCycle,
+      nearExitPreference, parkingCard,
+      this.doCharge,
+      this.chargingProvider,
+      this.chargeTime,
+      this.charge)
+      : super(key, name, licensePlate, height, width, length, turningCycle,
+            nearExitPreference, parkingCard);
+
+  static LoadableVehicle fromJson(Map<String, dynamic> parsedJson) {
+    List<DateTime> chargetime = [
+      parsedJson['chargetimebegin'],
+      parsedJson['chargetimeend']
+    ];
+
+    return new LoadableVehicle(
+        parsedJson['key'],
+        parsedJson['name'],
+        parsedJson['licenseplate'],
+        int.parse(parsedJson['height']),
+        int.parse(parsedJson['width']),
+        int.parse(parsedJson['length']),
+        int.parse(parsedJson['turningCycle']),
+        parsedJson['nearExitPreference'].parseBool(),
+        parsedJson['parkingCard'].parseBool(),
+        parsedJson['doCharge'].parseBool(),
+        parsedJson['chargingProvider'],
+        chargetime,
+        parsedJson['charge']);
+  }
+
+  //Todo
+  String getBatteryCharge() {}
+}
--- app/parkingapp/lib/models/classes/vehicle.dart
+++ app/parkingapp/lib/models/classes/vehicle.dart
@@ -1,69 +1,40 @@
 class Vehicle {
-  String key, name, licenseplate;
-  List<int> dimensions;
-  List<bool> preferences;
+  String key, name, licensePlate;
+  //Dimensions
+  int height, width, length, turningCycle;
+  //Preferences
+  bool nearExitPreference, parkingCard;
 
-  Vehicle(this.key, this.name, this.licenseplate, this.dimensions,
-      this.preferences);
+  Vehicle(
+      this.key,
+      this.name,
+      this.licensePlate,
+      this.height,
+      this.width,
+      this.length,
+      this.turningCycle,
+      this.nearExitPreference,
+      this.parkingCard);
 
   static Vehicle fromJson(Map<String, dynamic> parsedJson) {
-    List<int> dimensions = [
-      int.parse(parsedJson['dimensionH']),
-      int.parse(parsedJson['dimensionW']),
-      int.parse(parsedJson['dimensionD'])
-    ];
-    List<bool> preferences = [
-      parsedJson['nearExit'].parseBool(),
-      parsedJson['parkingcard'].parseBool()
-    ];
-    return new Vehicle(parsedJson['key'], parsedJson['name'],
-        parsedJson['licenseplate'], dimensions, preferences);
+    return new Vehicle(
+        parsedJson['key'],
+        parsedJson['name'],
+        parsedJson['licensePlate'],
+        int.parse(parsedJson['height']),
+        int.parse(parsedJson['width']),
+        int.parse(parsedJson['length']),
+        int.parse(parsedJson['turningCycle']),
+        parsedJson['nearExitPreference'].parseBool(),
+        parsedJson['parkingCard'].parseBool());
   }
+
+  //Todo
+  String setDimensions() {}
+
+  //Todo
+  String setPreferences() {}
 
   //Todo
   String toString() {}
-
-  //Todo
-  String setPreferences(List<bool> preferences) {}
-}
-
-class ElectricalVehicle extends Vehicle {
-  bool docharge;
-  String chargingprovider;
-  List<DateTime> chargetime;
-  String charge;
-
-  ElectricalVehicle(key, name, licenseplate, dimensions, preferences,
-      this.docharge, this.chargingprovider, this.chargetime, this.charge)
-      : super(key, name, licenseplate, dimensions, preferences);
-
-  static ElectricalVehicle fromJson(Map<String, dynamic> parsedJson) {
-    List<int> dimensions = [
-      int.parse(parsedJson['dimensionH']),
-      int.parse(parsedJson['dimensionW']),
-      int.parse(parsedJson['dimensionD'])
-    ];
-    List<bool> preferences = [
-      parsedJson['nearExit'].parseBool(),
-      parsedJson['parkingcard'].parseBool()
-    ];
-    List<DateTime> chargetime = [
-      parsedJson['chargetimebegin'],
-      parsedJson['chargetimeend']
-    ];
-
-    return new ElectricalVehicle(
-        parsedJson['key'],
-        parsedJson['name'],
-        parsedJson['licenseplate'],
-        dimensions,
-        preferences,
-        parsedJson['docharge'],
-        parsedJson['chargingprovider'],
-        chargetime,
-        parsedJson['charge']);
-  }
-
-  //Todo
-  String getBatteryCharge() {}
 }
