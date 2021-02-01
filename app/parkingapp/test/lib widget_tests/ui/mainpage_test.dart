2dc46ae3f3fef711344ca35bd72e28d6fb863a1e
Fabian <fabian_leibinger@gmx.de>
2021-02-01 17:18:41 +0100

    edited mainpage.dart widget test

--- app/parkingapp/test/lib widget_tests/ui/mainpage_test.dart
+++ app/parkingapp/test/lib widget_tests/ui/mainpage_test.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:parkingapp/main.dart';
+import 'package:parkingapp/models/global.dart';
+import 'package:parkingapp/routes/routes.dart';
 import 'package:parkingapp/ui/appdrawer/appdrawer.dart';
 import 'package:parkingapp/ui/mainpage/mainpage.dart';
 import 'package:parkingapp/enum/parkinggaragetype.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:parkingapp/ui/settingspage/settingspage.dart';
+import 'package:parkingapp/ui/vehiclepage/vehiclepage.dart';
 
 void main() {
-  testWidgets(
-      'mainpage has a specific appBar including title, appDrawer, text and a button',
-      (WidgetTester tester) async {
-    tester.pumpWidget(MainPage());
-
-    expect(find.byWidget(AppBar()), findsOneWidget);
-    //TODO replace 'vehicle'  by correct title
-    expect(find.text('vehicle'), findsOneWidget);
-
-    expect(find.byWidget(AppDrawer()), findsOneWidget);
-
-    expect(find.text(currentParkingGarage.name), findsOneWidget);
-    expect(
-        find.text(currentParkingGarage.type.toShortString()), findsOneWidget);
+  group('MainPage has specific AppBar, AppDrawer, text, button', () {
+    testWidgets('AppBar', (WidgetTester tester) async {
+      await tester.pumpWidget(AppDrawer());
   });
+});
 }
