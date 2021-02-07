1220e8525e7cf85ff51d09dc37f32812218a6108
Carl Hauschke <info@carlhauschke.de>
2021-02-07 22:18:35 +0100

    add editVehicle page

---
+++ app/parkingapp/lib/ui/editvehicle/editvehicle.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:parkingapp/models/global.dart';
+import 'package:parkingapp/routes/routes.dart';
+import 'package:parkingapp/ui/appdrawer/appDrawer.dart';
+
+class EditVehicle extends StatelessWidget {
+  static const String routeName = '/editVehicle';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Vehicle'),
+      ),
+      //replace with back button
+      drawer: AppDrawer(Routes.main),
+      body: VehicleForm(),
+    );
+  }
+}
+
+class VehicleForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _VehicleFormState();
+}
+
+class _VehicleFormState extends State<VehicleForm> {
+  //global key for form validation
+  final _formKey = GlobalKey<FormState>();
+
+  bool _vehicleChargeable = false;
+  String _name, _licensePlate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        key: _formKey,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextFormField(
+              autocorrect: false,
+              decoration: InputDecoration(labelText: 'Fahrzeugname'),
+              validator: (str) {},
+              onSaved: (str) => _name = str,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextFormField(
+              autocorrect: false,
+              decoration: InputDecoration(labelText: 'KFZ-Kennzeichen'),
+              validator: (str) {},
+              onSaved: (str) => _licensePlate = str,
+              inputFormatters: [UpperCaseTextFormatter()],
+            ),
+          ),
+          SwitchListTile(
+            title: Text('chargeable'),
+            onChanged: (bool newValue) =>
+                setState(() => _vehicleChargeable = newValue),
+            value: _vehicleChargeable,
+          ),
+          RaisedButton(
+            child: Text('Fahrzeug hinzufügen'),
+            onPressed: () => onPressed,
+          )
+        ],
+      ),
+    );
+  }
+
+  void onPressed() {
+    var form = _formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+    }
+  }
+}
+
+class UpperCaseTextFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    return TextEditingValue(
+      text: newValue.text?.toUpperCase(),
+      selection: newValue.selection,
+    );
+  }
+}
