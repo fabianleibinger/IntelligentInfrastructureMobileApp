1c7780dafdb6e4a10f9246f085752f9679adfd92
Fabian <fabian_leibinger@gmx.de>
2021-02-08 15:40:57 +0100

    chargingproviderdialog.dart commenting, deleted container

--- app/parkingapp/lib/dialogs/chargingproviderdialog.dart
+++ app/parkingapp/lib/dialogs/chargingproviderdialog.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:parkingapp/models/enum/chargingprovider.dart';
-import 'package:parkingapp/ui/mainpage/mainpage.dart';
 import 'constants.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+//defines charging provider dialog
 class ChargingProviderDialog extends StatefulWidget {
   @override
   _ChargingProviderDialogState createState() => _ChargingProviderDialogState();
@@ -14,15 +14,15 @@ class _ChargingProviderDialogState extends State<ChargingProviderDialog> {
   List<ChargingProvider> _providers = ChargingProvider.values;
   static ChargingProvider _selectedRadioTile;
 
+  //sets the initially selected tile
   @override
   initState() {
     super.initState();
     //TODO set vehicle provider to car provider
-    for (int i = 0; i < _providers.length; i++) {
-
-    }
+    for (int i = 0; i < _providers.length; i++) {}
   }
 
+  //saves the selected tile
   setSelectedRadioTile(ChargingProvider value) {
     setState(() {
       _selectedRadioTile = value;
@@ -33,29 +33,24 @@ class _ChargingProviderDialogState extends State<ChargingProviderDialog> {
   Widget build(BuildContext context) {
     return Constants.getConfirmationDialog(
         context,
-        AppLocalizations
-            .of(context)
-            .chargingProviderDialogTitle,
-        AppLocalizations
-            .of(context)
-            .settingsSaveButton,
+        AppLocalizations.of(context).chargingProviderDialogTitle,
+        AppLocalizations.of(context).settingsSaveButton,
         _getRadioListTiles(context));
   }
 
+  //returns a tile for every charging provider
   _getRadioListTiles(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          for (int i = 0; i < _providers.length; i++)
-            RadioListTile<ChargingProvider>(
-                title: Text(_providers[i].toShortString()),
-                value: _providers[i],
-                groupValue: _selectedRadioTile,
-                onChanged: (value) {
-                  setSelectedRadioTile(value);
-                })
-        ],
-      ),
+    return Column(
+      children: [
+        for (int i = 0; i < _providers.length; i++)
+          RadioListTile<ChargingProvider>(
+              title: Text(_providers[i].toShortString()),
+              value: _providers[i],
+              groupValue: _selectedRadioTile,
+              onChanged: (value) {
+                setSelectedRadioTile(value);
+              })
+      ],
     );
   }
 }
