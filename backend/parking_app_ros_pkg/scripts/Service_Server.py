e291eb03970620d3bf16c7debfce18f99514606c
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-02-12 16:19:19 +0100

    Add python script for ROS service for capacity

---
+++ backend/parking_app_ros_pkg/scripts/Service_Server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from parking_app_ros_pkg.srv import CapacityRequest, CapacityRequestResponse
+import rospy
+
+
+def handle_request_capacity(req):
+    print('Handle')
+    capacity_total = req.capacity.total
+    return capacity_total
+
+
+def request_capacity_server():
+    rospy.init_node('service_server')
+    s = rospy.Service('capacity_request', CapacityRequest, handle_request_capacity)
+    print("Ready to request capacity.")
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    request_capacity_server()
+    
