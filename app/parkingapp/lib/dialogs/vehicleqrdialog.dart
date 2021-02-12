76df5a72902ad645703f25dd66cc199ca0499f19
Fabian <fabian_leibinger@gmx.de>
2021-02-12 18:19:09 +0100

    added vehicle qr dialog

--- app/parkingapp/lib/dialogs/vehicleqrdialog.dart
+++ app/parkingapp/lib/dialogs/vehicleqrdialog.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:parkingapp/routes/routes.dart';
 import 'constants.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 //defines the qr code dialog
 class VehicleQRDialog {
-
+  static createVehicleQRDialog(BuildContext context) {
+    //TODO update next page
+    return Constants.createAlertDialog(
+        context,
+        AppLocalizations.of(context).vehicleQRDialogTitle,
+        AppLocalizations.of(context).vehicleQRDialogContent,
+        AppLocalizations.of(context).vehicleQRDialogCancelButton,
+        AppLocalizations.of(context).vehicleQRDialogConfirmButton,
+        Routes.settings);
+  }
 }
