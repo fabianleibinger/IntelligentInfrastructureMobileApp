010efb185ad352b8316f8732441bd4258c5b7dcf
sophie.witt <sophie.witt@student.kit.edu>
2021-02-13 19:51:29 +0100

    First idea for setting page

--- app/parkingapp/lib/ui/settingspage/settingspage.dart
+++ app/parkingapp/lib/ui/settingspage/settingspage.dart
@@ -9,6 +9,7 @@ import 'package:parkingapp/models/classes/user.dart';
 import 'package:parkingapp/models/global.dart';
 import 'package:wifi/wifi.dart';
 import 'package:parkingapp/ui/appdrawer/appdrawer.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatefulWidget {
   static const String routeName = '/settingspage';
@@ -24,7 +25,22 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: AppDrawer(),
-      body: Text("Hello"),
+      appBar: AppBar(title: Text('Einstellungen', style: whiteHeader)),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Language'),
+            leading: Icon(Icons.language),
+            onTap: () {},
+          ),
+          SettingsTile.switchTile(
+            title: 'Use fingerprint',
+            leading: Icon(Icons.fingerprint),
+            switchValue: true,
+            onToggle: (bool value) {},
+          ),
+        ],
+      ),
     );
   }
 }
