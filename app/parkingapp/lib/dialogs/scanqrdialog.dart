b12a8fbc7196831e775a03e16805cdf48aa350ce
Fabian <fabian_leibinger@gmx.de>
2021-02-16 16:06:54 +0100

    deleted unused dialog classes, moved vehicleqrdialog.dart to scanqrdialog.dart

--- app/parkingapp/lib/dialogs/scanqrdialog.dart
+++ app/parkingapp/lib/dialogs/scanqrdialog.dart
@@ -1,45 +1,18 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:parkingapp/models/global.dart';
+import 'package:parkingapp/routes/routes.dart';
 import 'constants.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ScanQRDialog extends StatefulWidget {
-  ScanQRDialog({Key key}) : super(key: key);
-
-  @override
-  _ScanQRDialogState createState() => _ScanQRDialogState();
-}
-
-class _ScanQRDialogState extends State<ScanQRDialog> {
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(Constants.padding),
-      ),
-      elevation: 0,
-      backgroundColor: Colors.transparent,
-      child: contentBox(context),
-    );
-  }
-
-  contentBox(context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(20),
-          height: 300,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[]),
-        ),
-      ],
-    );
+//defines the qr code dialog
+class ScanQRDialog {
+  static createVehicleQRDialog(BuildContext context) {
+    //TODO update next page
+    return Constants.createAlertDialog(
+        context,
+        AppLocalizations.of(context).vehicleQRDialogTitle,
+        AppLocalizations.of(context).vehicleQRDialogContent,
+        AppLocalizations.of(context).vehicleQRDialogCancelButton,
+        AppLocalizations.of(context).vehicleQRDialogConfirmButton,
+        Routes.settings);
   }
 }
