01335722e88bd1151475448127573a0b4e80c1a5
Fabian <fabian_leibinger@gmx.de>
2021-02-23 11:49:44 +0100

    parkinggarageoccupieddialog.dart: localizations

--- app/parkingapp/lib/dialogs/parkinggarageoccupieddialog.dart
+++ app/parkingapp/lib/dialogs/parkinggarageoccupieddialog.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:parkingapp/ui/mainpage/mainpage.dart';
-
 import 'constants.dart';
 
 //defines the dialog that tells the user that the parking garage is occupied
@@ -9,7 +8,7 @@ class ParkingGarageOccupiedDialog {
   static createDialog(BuildContext context) {
     return Constants.createAlertDialogOneButtonNoTitle(
         context,
-        'Leider sind gerade keine freien Parkplätze verfügbar.',
+        AppLocalizations.of(context).parkingGarageOccupiedDialogContent,
         AppLocalizations.of(context).parkDialogBackButton,
         vehicle.inAppKey);
   }
