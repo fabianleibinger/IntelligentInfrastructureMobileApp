052e92b8deac9a183db80c1c6934997edb3351bf
Fabian <fabian_leibinger@gmx.de>
2021-02-26 17:46:09 +0100

    finished setup_script.sh

--- backend/setup_script.sh
+++ backend/setup_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /opt/ros/%YOUR_ROS_DISTRO%/setup.bash
 roscore
 
 source /opt/ros/%YOUR_ROS_DISTRO%/setup.bash
@@ -7,7 +8,7 @@ catkin_make
 source devel/setup.bash
 rosrun parking_app_ros_pkg parking_app_flask_server.py
 
-source /opt/ros/noetic/setup.bash
+source /opt/ros/%YOUR_ROS_DISTRO%/setup.bash
 chmod u+x src/parking_app_ros_pkg/scripts/Service_Server.py
 catkin_make
 source devel/setup.bash
