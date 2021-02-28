e907936cfbc0de20e76f34a10762bc40429590b8
Carl Hauschke <info@carlhauschke.de>
2021-02-28 21:24:59 +0100

    add translations

--- app/parkingapp/lib/ui/FirstStart/termsofservice.dart
+++ app/parkingapp/lib/ui/FirstStart/termsofservice.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:parkingapp/ui/FirstStart/appconfiguration.dart';
-
-final String _tos =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh. Et egestas quis ipsum suspendisse ultrices. Sed tempus urna et pharetra pharetra massa massa ultricies. Elementum sagittis vitae et leo duis. Dui vivamus arcu felis bibendum ut tristique. Ac turpis egestas maecenas pharetra. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Ac ut consequat semper viverra nam libero justo laoreet sit. Venenatis a condimentum vitae sapien pellentesque habitant morbi. In arcu cursus euismod quis viverra nibh cras. Nunc faucibus a pellentesque sit amet. Id diam maecenas ultricies mi eget mauris. Tellus cras adipiscing enim eu turpis egestas pretium aenean. Ipsum consequat nisl vel pretium lectus quam id leo. Semper quis lectus nulla at volutpat diam ut. Leo vel orci porta non pulvinar neque laoreet suspendisse interdum. Cursus turpis massa tincidunt dui ut ornare lectus sit amet. Urna id volutpat lacus laoreet. Orci nulla pellentesque dignissim enim sit amet venenatis. Posuere sollicitudin aliquam ultrices sagittis orci a. Pretium fusce id velit ut tortor. Mauris ultrices eros in cursus turpis. Quis ipsum suspendisse ultrices gravida dictum. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Et magnis dis parturient montes nascetur ridiculus mus mauris vitae. Tortor posuere ac ut consequat semper. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. A cras semper auctor neque. Ultrices eros in cursus turpis massa tincidunt dui ut. Egestas pretium aenean pharetra magna. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla. Elementum tempus egestas sed sed risus pretium. Risus pretium quam vulputate dignissim suspendisse in. Et magnis dis parturient montes. Sit amet massa vitae tortor condimentum. Sed cras ornare arcu dui vivamus arcu. In metus vulputate eu scelerisque felis. Rhoncus urna neque viverra justo nec ultrices dui sapien eget. Pharetra et ultrices neque ornare aenean. Diam ut venenatis tellus in metus vulputate eu. Duis tristique sollicitudin nibh sit amet. Eget nulla facilisi etiam dignissim diam. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. Adipiscing elit duis tristique sollicitudin nibh. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. In ornare quam viverra orci sagittis eu volutpat odio. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Donec enim diam vulputate ut pharetra sit amet aliquam. Felis eget nunc lobortis mattis aliquam faucibus purus in. Mauris nunc congue nisi vitae suscipit tellus mauris. Purus ut faucibus pulvinar elementum. Nulla pellentesque dignissim enim sit. Ipsum nunc aliquet bibendum enim facilisis. Fames ac turpis egestas sed tempus urna et. Vitae turpis massa sed elementum tempus egestas sed sed risus. Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Varius quam quisque id diam vel. Mauris nunc congue nisi vitae suscipit tellus. Ut lectus arcu bibendum at varius vel pharetra. Commodo sed egestas egestas fringilla phasellus faucibus. Lobortis scelerisque fermentum dui faucibus. Non nisi est sit amet facilisis magna etiam tempor. Lacus luctus accumsan tortor posuere. Id neque aliquam vestibulum morbi blandit cursus risus at ultrices. Sit amet nisl purus in. Nisl pretium fusce id velit ut. Quam quisque id diam vel quam. A scelerisque purus semper eget. Nisl nisi scelerisque eu ultrices vitae. Pulvinar mattis nunc sed blandit libero volutpat. Nullam ac tortor vitae purus. Pretium nibh ipsum consequat nisl vel pretium lectus quam. In ante metus dictum at tempor commodo. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Et magnis dis parturient montes nascetur. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Pretium quam vulputate dignissim suspendisse in est ante. Elit sed vulputate mi sit. Sed lectus vestibulum mattis ullamcorper velit. Sapien eget mi proin sed libero enim sed. Libero enim sed faucibus turpis in. Pharetra convallis posuere morbi leo urna molestie at. Netus et malesuada fames ac turpis egestas maecenas. A arcu cursus vitae congue mauris. Turpis tincidunt id aliquet risus feugiat in ante metus dictum. Erat pellentesque adipiscing commodo elit at imperdiet dui. Eros donec ac odio tempor orci dapibus. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed. Ac tincidunt vitae semper quis. Nunc pulvinar sapien et ligula. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Tellus id interdum velit laoreet id donec ultrices. Faucibus turpis in eu mi bibendum neque egestas. Viverra maecenas accumsan lacus vel. Sagittis vitae et leo duis ut diam quam nulla. Sed nisi lacus sed viverra tellus in hac habitasse platea. Habitasse platea dictumst vestibulum rhoncus est. Ullamcorper a lacus vestibulum sed arcu non odio euismod. Duis ut diam quam nulla. Aliquam sem fringilla ut morbi. Sed id semper risus in hendrerit gravida rutrum quisque non. Urna nec tincidunt praesent semper. Sodales ut etiam sit amet nisl purus. Varius vel pharetra vel turpis nunc eget lorem. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Amet tellus cras adipiscing enim eu turpis egestas pretium. Interdum velit laoreet id donec. Egestas congue quisque egestas diam in arcu cursus euismod. Fusce id velit ut tortor pretium viverra suspendisse. Tempor nec feugiat nisl pretium fusce id velit ut. Ac feugiat sed lectus vestibulum mattis ullamcorper. Orci nulla pellentesque dignissim enim sit. Sagittis eu volutpat odio facilisis mauris sit. Vel risus commodo viverra maecenas accumsan lacus vel. Cursus sit amet dictum sit amet justo donec enim diam. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Venenatis lectus magna fringilla urna porttitor rhoncus. Sit amet commodo nulla facilisi nullam vehicula ipsum a. Vitae proin sagittis nisl rhoncus. Semper feugiat nibh sed pulvinar proin gravida hendrerit. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Turpis in eu mi bibendum neque egestas congue quisque. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Nullam vehicula ipsum a arcu cursus vitae congue mauris. Massa tincidunt nunc pulvinar sapien et. Viverra nam libero justo laoreet sit. Pulvinar neque laoreet suspendisse interdum consectetur. Mi proin sed libero enim sed faucibus turpis in. Integer enim neque volutpat ac. Id faucibus nisl tincidunt eget nullam. Leo vel orci porta non pulvinar neque laoreet. Aliquet enim tortor at auctor urna nunc id cursus. Fames ac turpis egestas sed tempus. Eu lobortis elementum nibh tellus. Sit amet massa vitae tortor. Quis auctor elit sed vulputate mi. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Elementum eu facilisis sed odio. Mattis aliquam faucibus purus in massa tempor nec feugiat. Cursus in hac habitasse platea dictumst. Elit pellentesque habitant morbi tristique senectus et netus et. In tellus integer feugiat scelerisque varius morbi enim. Pharetra massa massa ultricies mi quis hendrerit. Quisque non tellus orci ac auctor augue mauris augue neque. Et ligula ullamcorper malesuada proin libero. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Iaculis eu non diam phasellus vestibulum. At erat pellentesque adipiscing commodo elit at imperdiet dui. Blandit libero volutpat sed cras ornare. Cursus sit amet dictum sit amet justo. Feugiat pretium nibh ipsum consequat. Enim sit amet venenatis urna cursus eget nunc scelerisque viverra. Leo vel orci porta non pulvinar neque laoreet suspendisse interdum. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Massa enim nec dui nunc mattis enim. Hendrerit dolor magna eget est lorem. Gravida dictum fusce ut placerat orci nulla.';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class TermsOfService extends StatefulWidget {
   @override
@@ -17,35 +15,38 @@ class _TermsOfServiceState extends State<TermsOfService> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Herzlich Willkommen'),
+        title: Text(AppLocalizations.of(context).titleWelcome),
       ),
       bottomNavigationBar: Material(
         elevation: 10,
         child: ButtonBar(
           children: [
             FlatButton(
-              child: Text('Weiter'),
+              child: Text(AppLocalizations.of(context).buttonContinue),
               onPressed: () => _formKey.currentState.validate()
                   ? Navigator.push(
                       context,
                       MaterialPageRoute(
                           builder: (context) => AppConfiguration()))
-                  : setState(() {
-                      //_subtitle = 'Erforderlich';
-                    }),
+                  : null,
             )
           ],
         ),
       ),
       body: Column(
         children: [
-          Expanded(child: ListView(children: [Text(_tos)])),
+          //TOS text
+          Expanded(
+              child: ListView(
+                  children: [Text(AppLocalizations.of(context).terms)])),
+          //accept terms checkbox
           Form(
             key: _formKey,
             child: FormField(
               builder: (FormFieldState<dynamic> field) {
                 return CheckboxListTile(
-                  title: Text('Ich akzeptiere die AGBs'),
+                  title: Text(AppLocalizations.of(context).acceptTerms),
+                  //show that this field is required if in an incorrect state
                   subtitle: field.hasError
                       ? Text(
                           field.errorText,
@@ -62,7 +63,10 @@ class _TermsOfServiceState extends State<TermsOfService> {
                   value: _switch,
                 );
               },
-              validator: (value) => !_switch ? 'Erforderlich' : null,
+              //validate if the state is in the correct position
+              //if in off position return the error text
+              validator: (value) =>
+                  !_switch ? AppLocalizations.of(context).requiredText : null,
             ),
           ),
         ],
