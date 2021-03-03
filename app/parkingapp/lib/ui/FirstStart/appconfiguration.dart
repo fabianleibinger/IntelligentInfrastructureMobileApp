79360c8b1374236a13b9f214dddac1751eac21d5
sophie.witt <sophie.witt@student.kit.edu>
2021-03-03 15:19:23 +0100

    Added AppConfigurations

--- app/parkingapp/lib/ui/FirstStart/appconfiguration.dart
+++ app/parkingapp/lib/ui/FirstStart/appconfiguration.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:parkingapp/ui/FirstStart/addvehicle.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:parkingapp/ui/settingspage/changepasscodepage.dart';
+import 'package:shared_preferences_settings/shared_preferences_settings.dart';
 
 class AppConfiguration extends StatefulWidget {
   @override
@@ -8,6 +10,7 @@ class AppConfiguration extends StatefulWidget {
 }
 
 class _AppConfigurationState extends State<AppConfiguration> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +30,46 @@ class _AppConfigurationState extends State<AppConfiguration> {
           ),
         ),
         body: Center(
-          child: Text('TODO: Show settings page here'),
-        ));
+            child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ListView(
+                    children: <Widget>[
+                      Padding(padding: EdgeInsets.symmetric(vertical: 0.5)),
+                      SwitchSettingsTile(
+                        settingKey: 'pushNotifications',
+                        title: 'Push Nachrichten',
+                        subtitle: 'Hinweise erhalten, bei wichtigen Hinweisen',
+                      ),
+                      SwitchSettingsTile(
+                        settingKey: 'pushLoad',
+                        title: 'Ladevorgang',
+                        subtitle:
+                            'Push Benachrichtigungen zum Ladevorgang erhalten',
+                        visibleIfKey: 'pushNotifications',
+                      ),
+                      SwitchSettingsTile(
+                        settingKey: 'pushPark',
+                        title: 'Einparkvorgang',
+                        subtitle:
+                            'Push Benachrichtigungen bei abgeschlossenem Einparkvorgang',
+                        visibleIfKey: 'pushNotifications',
+                      ),
+                      ListTile(
+                        title: Text('Passwort'),
+                        subtitle: Text('App mit einem Passwort schützen'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      PasscodePage()));
+                        },
+                      ),
+                    ],
+                  ),
+                ))));
   }
 }
