8ea9d5a34216dd227a37624c15ec2135bc458ed2
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-03-03 12:02:31 +0100

    Adjust setup script to run roscore and Service_Server in background

--- backend/setup_script.sh
+++ backend/setup_script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+pip3 install -r requirements.txt
+
+roscore &
 
 chmod u+x src/parking_app_ros_pkg/bin/parking_app_flask_server.py
+chmod u+x src/parking_app_ros_pkg/scripts/Service_Server.py
 catkin_make
 source devel/setup.bash
+
+rosrun parking_app_ros_pkg Service_Server.py &
 rosrun parking_app_ros_pkg parking_app_flask_server.py
