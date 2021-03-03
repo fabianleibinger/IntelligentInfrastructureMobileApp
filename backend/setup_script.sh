e89be298ca3cccb82496352fc5098c8562836bda
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-03-03 12:53:59 +0100

    Restructure setup script to run ROS commands within one terminal
    Squashed commit of the following:
    
    commit 8ea9d5a34216dd227a37624c15ec2135bc458ed2
    Author: Bjarne Sauer <bjarne.sauer@student.kit.edu>
    Date:   Wed Mar 3 12:02:31 2021 +0100
    
        Adjust setup script to run roscore and Service_Server in background
    
    commit 594a02a045617276ca9971d185688214bedb92c6
    Author: Bjarne Sauer <bjarne.sauer@student.kit.edu>
    Date:   Tue Feb 23 10:53:40 2021 +0100
    
        Add requirements.txt for python imports
    
    commit 39182000600a049a1f8dac6a061525ef0f8d3b97
    Author: Bjarne Sauer <bjarne.sauer@student.kit.edu>
    Date:   Tue Feb 23 10:36:48 2021 +0100
    
        Add shell script for terminal commands setting up catkin and start server
    
    commit 69d9da908d173d32c1acc446a67363035b6c9be6
    Author: Bjarne Sauer <bjarne.sauer@student.kit.edu>
    Date:   Fri Feb 19 14:04:49 2021 +0100
    
        Add exception handling when ROS service is unavailable
    
        Furthermore, under route /connect information about the parking garage wifi are available
    
    commit 7ca22c3e03c381f74bd0259232b24d5595a4b2fc
    Author: Bjarne Sauer <bjarne.sauer@student.kit.edu>
    Date:   Fri Feb 19 13:03:18 2021 +0100
    
        Rename ParkApp_Flask_Server.py to parking_app_flask_server.py
    
        Naming convention for python modules. Furthermore, remove unused listener.py script (now using ROS services instead of Publisher/Subscriber)
    
    commit b0f0f1420dd42d92636d6987d0ce9c12a476630d
    Author: Bjarne Sauer <bjarne.sauer@student.kit.edu>
    Date:   Fri Feb 19 12:51:26 2021 +0100
    
        Create python package in ROS to implement helper methods in different python files
    
        Due to ROS' folder structure and python path, you cannot import python scripts within the 'scripts' foldet directly. Main files have to be stored under 'bin' and helper files in a package under 'src'. To make the files executable, a setup.py file can be run (changing CMakeLists so that it works automatically for catkin_make).
    
    commit 7116e34b441aa6a953182ad5891a578714d6d640
    Author: Bjarne Sauer <bjarne.sauer@student.kit.edu>
    Date:   Thu Feb 18 12:19:36 2021 +0100
    
        Restructure app.route fitting the format of CapacityRequest.msg
    
        route('/capacities') -> all parking spots in parking garage (free and occupied)
        route('/free') -> free parking spots in parking garage (incl. total, electric, electric_fast, electric_inductive)
        route('/free/total') -> free parking spots in total (one value)
        route('/free/electric') -> free electric parking spots (one value)

--- backend/setup_script.sh
+++ backend/setup_script.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
+pip3 install -r requirements.txt
 
-source /opt/ros/%YOUR_ROS_DISTRO%/setup.bash
-roscore
+roscore &
 
-source /opt/ros/%YOUR_ROS_DISTRO%/setup.bash
 chmod u+x src/parking_app_ros_pkg/bin/parking_app_flask_server.py
-catkin_make
-source devel/setup.bash
-rosrun parking_app_ros_pkg parking_app_flask_server.py
-
-source /opt/ros/%YOUR_ROS_DISTRO%/setup.bash
 chmod u+x src/parking_app_ros_pkg/scripts/Service_Server.py
 catkin_make
 source devel/setup.bash
-rosrun parking_app_ros_pkg Service_Server.py
+
+rosrun parking_app_ros_pkg Service_Server.py &
+rosrun parking_app_ros_pkg parking_app_flask_server.py
