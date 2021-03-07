fc3d960a185dd10ac1c0fca65f02d660950928c2
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-03-07 11:48:04 +0100

    Implement communicate_park_in in flask server and ROS service server

--- backend/parking_app_ros_pkg/scripts/Service_Server.py
+++ backend/parking_app_ros_pkg/scripts/Service_Server.py
@@ -3,11 +3,11 @@
 from __future__ import print_function
 
 from parking_app_ros_pkg.srv import CapacityRequest, CapacityRequestResponse
+from parking_app_ros_pkg.srv import RegisterVehicleRequest, RegisterVehicleRequestResponse
 import rospy
 
 
 def handle_request_capacity(req):
-    print('Handle')
     response = CapacityRequestResponse()
     response.capacity_total.total = 10
     response.capacity_free.total = 8
@@ -16,13 +16,20 @@ def handle_request_capacity(req):
     return response
 
 
-def request_capacity_server():
-    rospy.init_node('service_server')
-    s = rospy.Service('capacity_request', CapacityRequest, handle_request_capacity)
-    print("Ready to request capacity.")
+def handle_request_register(req):
+    response = RegisterVehicleRequestResponse()
+    response.vehicle_status.status = 1  # Parking in
+    response.pms_id = 222
+    return response
+
+
+def start_server():
+    rospy.init_node('service_server_capacity')
+    service_capacity = rospy.Service('capacity_request', CapacityRequest, handle_request_capacity)
+    service_parking = rospy.Service('register_vehicle_request', RegisterVehicleRequest, handle_request_register)
     rospy.spin()
 
 
 if __name__ == "__main__":
-    request_capacity_server()
-    
+    start_server()
+
