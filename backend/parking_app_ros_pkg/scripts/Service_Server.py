90256969cc51ffe866fb3a46438b6ae4d0c52044
Bjarne Sauer <bjarne.sauer@student.kit.edu>
2021-03-09 12:45:34 +0100

    Implement dummy in Service_Server for VehiclePositionRequest

--- backend/parking_app_ros_pkg/scripts/Service_Server.py
+++ backend/parking_app_ros_pkg/scripts/Service_Server.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 from parking_app_ros_pkg.srv import CapacityRequest, CapacityRequestResponse
 from parking_app_ros_pkg.srv import RegisterVehicleRequest, RegisterVehicleRequestResponse
+from parking_app_ros_pkg.srv import VehiclePositionRequest, VehiclePositionRequestResponse
 import rospy
 
 
@@ -20,10 +21,18 @@ def handle_request_register(req):
     response = RegisterVehicleRequestResponse()
     response.vehicle_status.status = 1  # Parking in
     response.pms_id = 222
-    response.target_parking_position.longitude = 8.42011294615
-    response.target_parking_position.latitude = 49.0143771428
+    response.target_parking_position.longitude = 8.4202020245
+    response.target_parking_position.latitude = 49.0141414141
     if req.info.loadable.load_during_parking:
         response.load_vehicle = True
+    return response
+
+
+def handle_request_vehicle_position(req):
+    response = VehiclePositionRequestResponse()
+    response.vehicle_status.status = 1
+    response.position.longitude = 8.42011294615
+    response.position.latitude = 49.01431771428
     return response
 
 
@@ -31,9 +40,11 @@ def start_server():
     rospy.init_node('service_server_capacity')
     service_capacity = rospy.Service('capacity_request', CapacityRequest, handle_request_capacity)
     service_parking = rospy.Service('register_vehicle_request', RegisterVehicleRequest, handle_request_register)
+    service_position = rospy.Service('vehicle_position_request', VehiclePositionRequest, handle_request_vehicle_position)
     rospy.spin()
 
 
 if __name__ == "__main__":
     start_server()
+
 
