029c1eff3b6cc5a676b1b67a04a595e99eca63fa
Fabian <fabian_leibinger@gmx.de>
2021-03-17 13:02:42 +0100

    restructuring apiprovider.dart

--- app/parkingapp/lib/bloc/resources/apiprovider.dart
+++ app/parkingapp/lib/bloc/resources/apiprovider.dart
@@ -6,7 +6,6 @@ import 'dart:convert';
 import 'package:parkingapp/models/classes/vehicle.dart';
 
 // defines the calls to the backend and what should happen with the results
-
 class ApiProvider {
   static final int httpGetStatusCodeSuccess = 200;
   static final int httpPostStatusCodeSuccess = 200;
@@ -61,6 +60,56 @@ class ApiProvider {
         _chooseParkInBody(vehicle), 'Failed to park in');
   }
 
+  //['parking', 'longitude', 'latitude']
+  //tries to park out vehicle or cancel park in
+  static Future<Map<String, dynamic>> parkOut(Vehicle vehicle) async {
+    return httpPost(
+        _serverUrl + _serverPort + "/parkOut",
+        _chooseParkOutBody(vehicle),
+        'Failed to park out');
+  }
+
+  //['longitude', 'latitude', 'moving', 'reached_position']
+  //tries to get position of the vehicle
+  static Future<Map<String, dynamic>> getPosition(Vehicle vehicle) async {
+    return httpPost(
+        _serverUrl + _serverPort + "/getPosition",
+        _chooseGetPositionBody(vehicle),
+        'Failed to get position');
+  }
+
+  //http get request that returns the response body
+  static Future<Map<String, dynamic>> httpGet(
+      String url, String failureText) async {
+    final response = await http.get(url).timeout(_timeOutAfter);
+    if (response.statusCode == httpGetStatusCodeSuccess) {
+      final Map result = json.decode(response.body);
+      print(result.entries.toString());
+      return result;
+    } else {
+      throw Exception(failureText);
+    }
+  }
+
+  //http post request that returns the response body
+  static Future<Map<String, dynamic>> httpPost(
+      String url, Map<String, dynamic> body, String failureText) async {
+    final response = await http
+        .post(url,
+            headers: <String, String>{
+              _httpPostHeaderFirst: _httpPostHeaderSecond,
+            },
+            body: jsonEncode(body))
+        .timeout(_timeOutAfter);
+    if (response.statusCode == httpGetStatusCodeSuccess) {
+      final Map result = json.decode(response.body);
+      print(result.entries.toString());
+      return result;
+    } else {
+      throw Exception(failureText);
+    }
+  }
+
   //chooses and returns http post body for park in call
   static Map<String, dynamic> _chooseParkInBody(Vehicle vehicle) {
     if (vehicle.runtimeType == ChargeableVehicle) {
@@ -101,61 +150,20 @@ class ApiProvider {
     };
   }
 
-  //['parking', 'longitude', 'latitude']
-  //tries to park out vehicle or cancel park in
-  static Future<Map<String, dynamic>> parkOut(Vehicle vehicle) async {
-    return httpPost(
-        _serverUrl + _serverPort + "/parkOut",
-        {
-          "id": vehicle.databaseId,
-          "number_plate": vehicle.licensePlate,
-          "length": vehicle.length,
-          "width": vehicle.width,
-          "turning_radius": vehicle.turningCycle,
-          "dist_rear_axle_numberplate": vehicle.distRearAxleLicensePlate,
-        },
-        'Failed to park out');
+  //chooses and returns http post body for park out call
+  static Map<String, dynamic> _chooseParkOutBody(Vehicle vehicle) {
+    return {
+      "id": vehicle.databaseId,
+      "number_plate": vehicle.licensePlate,
+      "length": vehicle.length,
+      "width": vehicle.width,
+      "turning_radius": vehicle.turningCycle,
+      "dist_rear_axle_numberplate": vehicle.distRearAxleLicensePlate,
+    };
   }
 
-  //TODO add body
-  //['longitude', 'latitude', 'moving', 'reached_position']
-  //tries to get position of the vehicle
-  static Future<Map<String, dynamic>> getPosition(Vehicle vehicle) async {
-    return httpPost(
-        _serverUrl + _serverPort + "/getPosition",
-        {"id": vehicle.inAppKey, "number_plate": vehicle.licensePlate},
-        'Failed to get position');
-  }
-
-  //http get request that returns the response body
-  static Future<Map<String, dynamic>> httpGet(
-      String url, String failureText) async {
-    final response = await http.get(url).timeout(_timeOutAfter);
-    if (response.statusCode == httpGetStatusCodeSuccess) {
-      final Map result = json.decode(response.body);
-      print(result.entries.toString());
-      return result;
-    } else {
-      throw Exception(failureText);
-    }
-  }
-
-  //http post request that returns the response body
-  static Future<Map<String, dynamic>> httpPost(
-      String url, Map<String, dynamic> body, String failureText) async {
-    final response = await http
-        .post(url,
-            headers: <String, String>{
-              _httpPostHeaderFirst: _httpPostHeaderSecond,
-            },
-            body: jsonEncode(body))
-        .timeout(_timeOutAfter);
-    if (response.statusCode == httpGetStatusCodeSuccess) {
-      final Map result = json.decode(response.body);
-      print(result.entries.toString());
-      return result;
-    } else {
-      throw Exception(failureText);
-    }
+  //chooses and returns http post body for park out call
+  static Map<String, dynamic> _chooseGetPositionBody(Vehicle vehicle) {
+    return {"id": vehicle.inAppKey, "number_plate": vehicle.licensePlate};
   }
 }
