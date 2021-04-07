4341843b7750b414922797d66ca624e991af059b
Carl Hauschke <Carl@carls-macbook-pro.local>
2021-04-07 18:17:03 +0200

    group generateTile

--- app/parkingapp/test/ui/appdrawer/appdrawer_test.dart
+++ app/parkingapp/test/ui/appdrawer/appdrawer_test.dart
@@ -3,9 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:parkingapp/ui/appdrawer/appdrawer.dart';
 
 void main() {
-  test('tests the highlighting of a tile. This tile should be selected', () {
-    var tile = generateTile(null, 'drawer', 'drawer', 'Title', Icons.ac_unit);
-    expect(tile.selected, true);
+  group('test regular tiles', () {
+    test('tests the highlighting of a tile. This tile should be selected', () {
+      var tile = generateTile(null, 'drawer', 'drawer', 'Title', Icons.ac_unit);
+      expect(tile.selected, true);
+    });
+
+    test('tests the highlighting of a tile. This tile should notbe selected',
+        () {
+      var tile =
+          generateTile(null, 'currentDrawer', 'drawer', 'Title', Icons.ac_unit);
+      expect(tile.selected, false);
+    });
+  });
   });
 
   test('tests the highlighting of a tile. This tile should notbe selected', () {
